4dc624f1be6c4191001a2380cd3a9a5952b212fc
Roger Kondrat <misterhtmlcss@gmail.com>
2020-09-21 13:06:18 -0600

    todo list basics setup pt2

---
+++ W3/W3D1/shared/todo-list/src/index.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+
+// Demo Inside - Start
+
+const Todos = (props) => {
+
+  // Code you know:
+  // Example, but not general practice.
+  // const todoThingies = useState('')
+  // const todo = todoThingies[0]
+  // const setTodo = todoThingies[1]
+
+  const [todos, setTodos] = useState([])
+  const [newtodo, setTodo] = useState('')
+
+  const addTodo = event => {
+    // Prevent page reload on click
+    event.preventDefault()
+    setTodo(event.target.value)
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(function (todo) {
+      return todo.id !== id
+    }))
+  }
+
+  const handleSubmit = event => {
+    // Prevent page reload on submit
+    event.preventDefault()
+    // Is there any data in the Todo field to submit?
+    if(newtodo === '') return
+
+    // There is enough data, spread in the existing todos and
+    // add a new todo
+    setTodos(
+    [
+      ...todos,
+      {
+      id: Date.now(),
+      todo: newtodo,
+      done: false
+      }
+    ])
+    // Reset input field
+    event.target.reset()
+  }
+
+  return (
+    <div>
+      <header>
+        <h1>Todos App</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={addTodo}/>
+          <button type="submit" onSubmit={handleSubmit}>AddTodo</button>
+        </form>
+      </header>
+      <main>
+        <ul>
+          {
+            todos.map(({id, todo, done}, idx) => {
+              return (
+                <li
+                  key={id}
+                  onClick={()=> deleteTodo(id)}
+                >
+                  {todo}
+                </li>
+              )
+            })
+          }
+        </ul>
+      </main>
+    </div>
+  )
+}
+
+// Demo Outside - End
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Todos />
+  </React.StrictMode>
+  ,
+  document.getElementById('root')
+);
+
+
